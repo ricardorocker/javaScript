9eb19d9eef905ca31a800f1083c351fe4a974059
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-07-29 23:45:40 -0300

    Começo Repetições

---
+++ 11-Repeticoes.js
@@ -0,0 +1,6 @@
+var metodoManual = document.getElementById("metodoManual");
+
+metodoManual.innerHTML = "Essa é a vez 1 <br> Essa é a vez 2 <br> Essa é a vez 3 <br> Essa é a vez 4 <br> Essa é a vez 5 <br> ";
+
+
+
