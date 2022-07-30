7069825a3b9138b1d90f1811d87206bc7fe948fe
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-07-30 01:00:55 -0300

    Finalizando uso pela primeira vez de todas repetições

--- 11-Repeticoes.js
+++ 11-Repeticoes.js
@@ -1,6 +1,48 @@
+// Método Manual
 var metodoManual = document.getElementById("metodoManual");
+metodoManual.innerHTML = 
+"Essa é a 1ª vez digitando manualmente<br> " + 
+"Essa é a 2ª vez digitando manualmente<br> " +
+"Essa é a 3ª vez digitando manualmente<br> " + 
+"Essa é a 4ª vez digitando manualmente<br> " + 
+"Essa é a 5ª vez digitando manualmente<br> ";
 
-metodoManual.innerHTML = "Essa é a vez 1 <br> Essa é a vez 2 <br> Essa é a vez 3 <br> Essa é a vez 4 <br> Essa é a vez 5 <br> ";
+// Método com While
+var metodoWhile = document.getElementById("metodoWhile");
+var toAdd = document.createDocumentFragment();
+var i = 1;
+while(i < 6){
+    var newDiv = document.createElement("div");
+    newDiv.id = "metodoWhile" + i;
+    newDiv.className = "text-aline";
+    newDiv.innerHTML = `Essa é a ${i}ª vez usando o While`
+    metodoWhile.appendChild(newDiv);
+    i++;
+}
+
+// Método com Do-While
+var metodoDoWhile = document.getElementById("metodoDoWhile");
+var toAdd = document.createDocumentFragment();
+var i = 1;
+    do {
+    var newDiv = document.createElement("div");
+    newDiv.id = "metodoDoWhile" + i;
+    newDiv.className = "text-aline";
+    newDiv.innerHTML = `Essa é a ${i}ª vez usando o Do-While`
+    metodoDoWhile.appendChild(newDiv);
+    i++;
+} while(i < 6);
+
+// Método com For
+var metodoFor = document.getElementById("metodoFor")
+var toAdd = document.createDocumentFragment();
+for (i = 1 ; i < 6 ; i++){
+    var newDiv = document.createElement("div");
+    newDiv.id = "metodoFor" + i;
+    newDiv.className = "text-aline";
+    newDiv.innerHTML = `Essa é a ${i}ª vez usando o For`
+    metodoFor.appendChild(newDiv);
+}
 
 
 
