f2cc41a86d2c6b6c81d58413d7dd12682ddc6b6e
ricardorocker <ricardo.santos.rocker@gmail.com>
2022-08-07 00:45:27 -0300

    Arquivos js de cada aula criado separamente

---
+++ 01-Introducao.js
@@ -0,0 +1,5 @@
+// window.alert("Mensagem de windows.alert")
+// window.confirm("Mensagem com confirmação window.confirm")
+var nome = window.prompt("Qual é o seu nome?");
+
+window.alert("Olá " + nome);
