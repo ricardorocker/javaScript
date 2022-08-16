217427dbd32dce6a50ccf3f958a9e657e657b694
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-08-16 14:53:41 -0300

    DOM - Eventos part01

---
+++ dom/02-eventosDom.js
@@ -0,0 +1,9 @@
+function changeParagrafro(i){
+    let paragrafo = document.getElementById("p1");
+    // paragrafo.innerText = document.getElementsByTagName("input")[0].value;
+    paragrafo.innerText = i.value;
+}
+
+function hideButton(i){
+    i.style.display = "none";
+}
