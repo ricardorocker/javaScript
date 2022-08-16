2917a96c4caabf43ba438912d67e9f5191af6096
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-08-16 15:20:29 -0300

    DOM - Eventos part02

---
+++ dom/03-eventosDom.js
@@ -0,0 +1,8 @@
+function overTitle(elemento){
+    elemento.style.color = "white";
+    elemento.style.backgroundColor = "black";
+}
+
+function outTitle(elemento){
+    elemento.style.backgroundColor = "green";
+}
