93553cc90ed492002c1ce0280db2654bc223db88
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-09-19 15:33:36 -0300

    Reduce

---
+++ higher order functions/06-reduce.js
@@ -0,0 +1,24 @@
+let aluno1 = {
+    nome: "Ricardo",
+    idade: 27
+}
+
+let aluno2 = {
+    nome: "Petrucia",
+    idade: 25
+}
+
+let aluno3 = {
+    nome: "Amanda",
+    idade: 22
+}
+
+let alunos = [aluno1, aluno2, aluno3];
+
+console.log("alunos", alunos);
+
+function somaIdadeTurma(total, aluno){
+    return total + aluno.idade;
+}
+
+console.log("Reduce alunos", alunos.reduce(somaIdadeTurma, 0))
